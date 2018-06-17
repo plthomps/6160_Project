a0a9d7f3a0e6919ff226dca91033f5356c64107f
swathark <swathark@uncc.edu>
2018-06-17 16:50:41 -0400

    Header and footer files

---
+++ projectCode/html/footer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Pet Inn</title>
+		<meta charset="utf-8">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">-->
+		<!--<link type="text/css" rel="stylesheet" href="../css/style.css" />-->
+	</head>
+	<style>
+	.footer{
+		padding: 5px;
+		position: fixed;
+	    left: 0;
+	    bottom: 0;
+	    width: 100%;
+	    text-align: center;
+		background-color:#66666642;
+	}
+	/*.footerCls .row {
+			margin-right: 0px;
+			margin-left: 0px;
+		}*/
+	</style>
+	<body class="footerCls">
+	<center>
+		<div class="row footer">
+			<div class="col-md-12 col-lg-12">
+				<center><b> © TeamRover 2018 | Don't produce any of the works without written consent | Designed and developed by TeamRover </b></center>
+			</div>
+		</div>
+	</center>
+	</body>
+</html>
