0a1d42126388eb7fc1a7bb254d6d1fa446c71ccf
prashantdesai93 <39637879+prashantdesai93@users.noreply.github.com>
2018-06-22 18:04:10 -0400

    Added Project folder

---
+++ ProjectTurnIN/ProjectCode/html/deletePet.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+
+DELETE.PHP
+
+*/
+
+
+
+// connect to the database
+
+$link = mysqli_connect("db-master.c2rtzjxij2h6.us-east-2.rds.amazonaws.com", "6160_team_member", "team6160_pwnew3452", "roverdb6160");
+		 
+		// Check connection
+		if($link === false){
+			//die("ERROR: Could not connect. " . mysqli_connect_error());
+		} else {
+			//echo "Connected...";
+		}
+
+
+
+// check if the 'id' variable is set in URL, and check that it is valid
+
+if (isset($_GET['id']) && is_numeric($_GET['id']))
+
+{
+
+// get id value
+
+$id = $_GET['id'];
+
+//echo $id;
+
+// delete the entry
+
+ if(mysqli_query($link, "DELETE FROM pet WHERE petid=$id")){
+	//echo "<script type='text/javascript'>alert('Pet Deleted successfully.');</script>";
+  	header("Location: dashboard.php");
+	} else{
+	echo "<script type='text/javascript'>alert('Delete order for this pet first then delete the pet.');</script>";}
+  	header("Location: dashboard.php");
+
+}
+
+
+?>
