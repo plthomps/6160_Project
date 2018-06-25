f8fb950900b500aacaac45da365bf96c5a2c166d
Taylor Conners <tconners@uncc.edu>
2018-06-25 00:08:21 -0400

    cleaned repo, added sql dump

--- projectCode/html/access.php
+++
@@ -1,4 +0,0 @@
-<!--Author:Taylor Conners-->
-<?php
-    $db = new PDO("mysql:host=db-master.c2rtzjxij2h6.us-east-2.rds.amazonaws.com;dbname=roverdb6160", "6160_team_member", "team6160_pwnew3452", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-?>
